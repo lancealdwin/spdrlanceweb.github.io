3e464e0707c540f46a86a1064090caf8dfc651f0
lancealdwin <lancealdwin.s02@gmail.com>
2017-02-28 18:28:57 +0800

    Add files via upload

---
+++ jslance.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+	
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
+	
+	$(document).ready(function(){
+      $('.slider').slider({fullWidth: true});
+    });
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });//for zooming photos
+  
+   $('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
